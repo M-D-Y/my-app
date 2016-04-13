f530b55b3edabcabece4b42e6991d328107059ff
M-D-Y <muratovd@yandex.ru>
2016-04-14 01:58:04 +0300

    1

---
+++ lesson/src/main/java/lesson/CalcTest.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package lesson;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * @author admin
+ *
+ * @since 14 апр. 2016 г.
+ */
+public class CalcTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
